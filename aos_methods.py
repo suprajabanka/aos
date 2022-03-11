679831399782c4b38e03dc614a82ad71dbb08748
suprajabanka <suprajaramakrishna@gmail.com>
2022-03-11 14:35:07 -0800

    Added selenium scripts

--- aos_methods.py
+++ aos_methods.py
@@ -6,9 +6,21 @@ from time import sleep
 import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 
-s = Service(executable_path='c:\\Users\\ramak\\PycharmProjects\\pythonProject\\chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+#s = Service(executable_path='c:\\Users\\ramak\\PycharmProjects\\pythonProject\\chromedriver.exe')
+#driver = webdriver.Chrome(service=s)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 
 def setup():
     print(f'launch {locators.website} Home Page')
